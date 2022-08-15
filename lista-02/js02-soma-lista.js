5bd4629578d343c6e92d833b82ac8fc45c4cd0cb
Marcela Lima <marcelalima.df@gmail.com>
2022-08-15 10:24:27 -0300

    exercício 2, lista 2

--- lista-02/js02-soma-lista.js
+++ lista-02/js02-soma-lista.js
@@ -12,5 +12,7 @@ var entrada = [1,3,5,7,8,10]
 var soma = 0
 
 // percorrer a lista e fazer a somatória dos elementos
-
+for (var i = 0; i < entrada.length; i++) {
+    soma += entrada [i];
+}
 console.log(soma)
