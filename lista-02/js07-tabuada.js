302a5cb3a8ec153377f4a611630aff30b1cdf8ee
Marcela Lima <marcelalima.df@gmail.com>
2022-08-15 11:35:57 -0300

    exercício 07, lista 2

--- lista-02/js07-tabuada.js
+++ lista-02/js07-tabuada.js
@@ -18,6 +18,16 @@
 // Entrada
 var entrada = 5
 
-var tabuada = [5, 10, 15]
+var tabuada = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-console.log(tabuada)
+
+
+if (entrada < 1 || entrada > 10) {
+    console.log("Entrada Inválida")
+}
+else{
+console.log(`a tabuadada de ${entrada} é:`)
+   for (i of tabuada) {
+    console.log(entrada*i)
+   }
+}
