c743e33b5f2a2d23a34235902cf690db93080549
Marcela Lima <marcelalima.df@gmail.com>
2022-08-15 11:14:28 -0300

    exercício 05, lista 2

--- lista-02/js05-max-2.js
+++ lista-02/js05-max-2.js
@@ -13,8 +13,23 @@
 
 // Entrada
 var entrada = [40,42,1,0,-10]
-
 var segundoMaior = 40
 
+maior = 0
+for (i of entrada) {
+    if (i > maior) {
+        maior = i
+    }
+}
 
-console.log(segundoMaior)
+segundo = 0
+for (s of entrada) {
+    if (s < maior && s > segundo)
+    segundo = s
+}
+
+console.log(`O maior número é ${maior}`)
+console.log(`O segundo maior número é ${segundo}`)
+
+
+
