94faefda6d006f5e2a515a40eb37f2f848f74255
Marcela Lima <marcelalima.df@gmail.com>
2022-08-15 14:29:23 -0300

    exercício 09, lista 2

--- lista-02/js09-seis-por-dois.js
+++ lista-02/js09-seis-por-dois.js
@@ -12,3 +12,44 @@
 // Pŕoxima volta
 
 // Saída com base na próxima folga, calcular dia de trabalho/folga
+// date =  new Date()
+// i = 1
+
+// while (i < 90) {
+    // trabalho = new Date(date.setDate(date.getDate()+6))
+    // console.log(trabalho)
+//     folga = new Date(date.setDate(date.getDate()+1))
+//     console.log(folga.toDateString())
+//     folga = new Date(date.setDate(date.getDate()+1))
+//     console.log(folga.toDateString()+1)
+//     i = i+8
+// }
+
+var diaAtual = new Date();
+
+
+for (m = 0; m < 12; m++) {
+    console.log("\nDias Úteis:");
+
+    for (i = 0; i < 6; i++) {
+        diaAtual.setDate(diaAtual.getDate() + 1);
+    console.log(diaAtual.toLocaleDateString());
+
+    }
+  
+
+  console.log("\nDias de Folga:");
+
+
+    for (i = 0; i < 2; i++) {
+        diaAtual.setDate(diaAtual.getDate() + 1);
+        console.log(diaAtual.toLocaleDateString());
+        
+    }
+}  
+
+
+
+
+
+
